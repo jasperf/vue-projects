65175458adeda20afdd110abcd12c62b4ef24d92
Jasper Frumau <jasper@imagewize.com>
2017-09-03 14:02:50 +0300

    added isVisble and directive v-show

--- component-w-message/js/main.js
+++ component-w-message/js/main.js
@@ -1,20 +1,31 @@
 // Component must be registered before Vue instance
 Vue.component('message', { 
 	props: ['title', 'body'], 
+	data() {
+		return {
+		isVisible: true
+	};
+	},
 	// props here not inside the params for the component!
 	//  As you can see you can use more custom tags this way
 	//  No need to create one component per custom html tag
 	template: `
-		<article class="message">
+		<article class="message" v-show="isVisible">
 		  <div class="message-header">
 		    {{title}}
-		    <button class="delete" aria-label="delete"></button>
+		    <button class="delete" @click="hideModal" aria-label="delete"></button>
 		  </div>
 		  <div class="message-body">
 		  {{body}}
 		  </div>
 		</article>
-	`
+	`,
+
+	methods: {
+		hideModal() {
+			this.isVisible =  false;
+		}
+	}
 });
 
 new Vue({
