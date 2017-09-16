285525807da36f9a951d8d4240b831200b10f56d
Jasper Frumau <jasper@imagewize.com>
2017-09-16 16:24:40 +0300

    add method completeTodo(todo)

--- todo-app/src/components/Todo.vue
+++ todo-app/src/components/Todo.vue
@@ -39,7 +39,7 @@
     <div class='ui bottom attached green basic button' v-show="!isEditing &&todo.done" disabled>
         Completed
     </div>
-    <div class='ui bottom attached red basic button' v-show="!isEditing && !todo.done">
+    <div class='ui bottom attached red basic button' v-on:click="completeTodo(todo)" v-show="!isEditing && !todo.done">
         Pending
     </div>
   </div>
--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div style="margin-top:20px;">
     <!-- <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p> -->
    <!-- // we are now passing the data to the todo component to render the todo list -->
    <!-- see below the delete-todo which will send the order to the Todo component -->
-    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" v-bind:todo="todo"></todo>
+    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :todo.sync="todo"></todo>
   </div>
 </template>
 
 <script type = "text/javascript" >
-
+import sweetalert from 'sweetalert'
 import Todo from './Todo'
 
 export default {
@@ -25,10 +25,12 @@ export default {
       const todoIndex = this.todos.indexOf(todo)
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
       this.todos.splice(todoIndex, 1)
+      sweetalert('Deleted!', 'Your To-Do has been deleted.', 'success')
     },
     completeTodo (todo) {
       const todoIndex = this.todos.indexOf(todo)
       this.todos[todoIndex].done = true
+      sweetalert('Success!', 'To-Do completed!', 'success')
     }
   }
 }
